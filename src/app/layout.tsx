284f33899afe1311562d74e8df818755019e39e9
jvfontouraj <jvfontouraj@gmail.com>
2025-08-06 21:09:49 -0300

    add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import WppVerde from '@/assets/wpp verde.png'
 import { GoogleTagManager } from '@next/third-parties/google'
+import Script from 'next/script'
 
 const figtree = Figtree({
   variable: '--font-geist-sans',
@@ -28,7 +29,27 @@ export default function RootLayout({
     <>
       <GoogleTagManager gtmId="GTM-TT4C7FLQQ" />
       <html lang="pt">
+        <head>
+          {/* Google Tag Manager */}
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MGW2X2TS');
+          `}
+          </Script>
+        </head>
         <body className={`${figtree.variable} relative antialiased`}>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MGW2X2TS"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+      `,
+            }}
+          />
           <Header />
           {children}
           <Link
